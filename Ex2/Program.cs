1bcb10b88bc5284473135d66a1cff46e1d274678
Dmytro Kyba <dkyba2006@gmail.com>
2023-09-14 11:38:03 +0300

    Done Ex2

---
+++ Ex2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(Math.Pow(2, x) * x * Math.Cos(x) +1);
+
+            Console.ReadKey();
+        }
+    }
+}
