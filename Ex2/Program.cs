3c2bceed4caa3ff8025e9d07cb7a9aeae7e9ef58
Dmytro Kyba <dkyba2006@gmail.com>
2023-09-14 14:50:04 +0300

    Added the fix to Ex2 to pass the tests

--- Ex2/Program.cs
+++ Ex2/Program.cs
@@ -6,10 +6,11 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            Console.Write("Введіть х: ");
+            System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+            // Console.Write("Введіть х: ");
             double x = Convert.ToDouble(Console.ReadLine());
             double y = Math.Pow(2, x) * x * Math.Cos(x) + 1;
-            Console.WriteLine($"y = {y}");
+            Console.WriteLine(y);
 
             Console.ReadKey();
         }
